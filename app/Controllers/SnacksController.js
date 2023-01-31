56bcb494ec4bc31bdc31f38da5af6ace5659484b
Taylor Smith <smith.taylord@gmail.com>
2023-01-31 15:19:57 -0700

    money draws, template for HTML Snacks Set

--- app/Controllers/SnacksController.js
+++ app/Controllers/SnacksController.js
@@ -1,7 +1,30 @@
+import { appState } from "../AppState.js";
+import { snacksService } from "../Services/SnacksService.js";
+import { setText } from "../Utils/Writer.js";
+
+function _drawMoney() {
+    let money = appState.money
+    let template = 'Total Money: $0.00'
+    for (let i = 0; i < money; i++) {
+        template = `Total Money: $${money}`
+    }
+    setText('money', template)
+}
+
+function_drawSnacks(){
 
 
+}
 export class SnacksController {
-    constructor()
+    constructor() {
+        _drawMoney()
+        appState.on('money', _drawMoney)
+    }
+
+    addMoney() {
+        snacksService.addMoney()
+
+    }
 
 
 }
